2e4fd860d3e825709b0dbb8744986d1bc5387db4
dallas <dboyer@dexter.com>
2023-04-06 05:40:47 -0500

    Feature: added a test component in preparation of integrating redux

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,13 @@
+import { Fragment } from "react";
+import Test from "./Test";
+
 const App = () => {
-  return <h1>Employee Polls</h1>
+  return (
+    <Fragment>
+      <h1>Employee Polling</h1>
+      <Test />
+    </Fragment>
+  );
 }
 
 export default App;
---
+++ src/components/Test.js
@@ -0,0 +1,6 @@
+const Test = () => {
+    return <h1>test</h1>
+}
+  
+export default Test;
+  
