91bea4ff53a9f8355959990391080d3fbf530cb0
dallas <dboyer@dexter.com>
2023-04-10 16:20:28 -0500

    Refactor: added leaderboard route and updated dashboard route

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import {Route, Routes} from "react-router-dom";
 import Nav from "./Nav";
 import Login from "./Login";
 import Dashboard from "./Dashboard";
+import Leaderboard from "./Leaderboard";
 
 const App = () => {
   return (
@@ -10,9 +11,21 @@ const App = () => {
       <Nav />
       <div className="container">
         <Routes>
-          <Route path="/login" element={<Login />} />
+          <Route
+            path="/login"
+            element={<Login />}
+          />
 
-          <Route path="/dashboard" element={<Dashboard />} />
+          <Route
+            path="/"
+            exact
+            element={<Dashboard />}
+          />
+
+          <Route
+            path="/leaderboard"
+            element={<Leaderboard />}
+          />
         </Routes>
       </div>
     </Fragment>
