e7c969b2644a746ffabd9e3a8ed6b61ff6e6f3f4
dallas <dboyer@dexter.com>
2023-04-10 18:43:37 -0500

    Feature: added base for authenticating users on login page

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,13 +1,40 @@
-import { Fragment } from "react";
+import {
+    Fragment,
+    useState
+} from "react";
 
-const Login = () =>{
+const Login = () => {
+
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleChange = (e) => {
+        const text = e.target.value;
+        const option = e.target.id;
+    
+        option === "username" ? setUsername(text) : setPassword(text);
+    };
+
+    console.log("Username: ", username);
+    console.log("Password: ", password);
+
     return (
         <Fragment>
             <img src="" alt="Default Avatar" />
 
             <form>
-                <input type="text" placeholder="Enter Username" />
-                <input type="password" placeholder="Enter Password" />
+                <input
+                    type="text"
+                    placeholder="Enter Username"
+                    onChange={handleChange}
+                />
+
+                <input
+                    type="password"
+                    placeholder="Enter Password"
+                    onChange={handleChange}
+                />
+
                 <button onClick={(e) => {
                     e.preventDefault();
                     console.log("Form Submitted")
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux";
 import tests from "./tests";
+import authedUser from "./authedUser";
 
 export default combineReducers({
-    tests
+    tests,
+    authedUser
 });
