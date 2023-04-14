9cfd1359e373e9540ab0bdb2bb76c6de4471955c
dallas <dboyer@dexter.com>
2023-04-14 06:03:28 -0500

    Refactor: build-out of base poll functionality

--- src/components/Poll.js
+++ src/components/Poll.js
@@ -1,5 +1,23 @@
-const Poll = () => {
-    return <h1>this is a poll</h1>
+import { connect } from "react-redux";
+
+const Poll = ({ poll, author }) => {
+
+    console.log("This Poll: ", poll);
+    
+    return (
+        <div className="poll-container">
+            <h1>Poll create by {author}</h1>
+        </div>
+    );
 }
 
-export const Poll;
+const mapStateToProps = ({ polls, author }, { id }) => {
+    const poll = polls[id];
+
+    return {
+        poll: poll,
+        author: poll ? users[poll.author] : null
+    };
+}
+
+export default connect(mapStateToProps)(Poll);
