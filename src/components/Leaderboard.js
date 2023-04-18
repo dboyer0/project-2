72acca636702a701ab21510c6aa1086e9a7d5bb1
dallas <dboyer@dexter.com>
2023-04-18 13:29:20 -0500

    Refactor: finish building out the leaderboard

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -1,5 +1,51 @@
-const Leaderboard = () =>{
-    return <h1>Leaderboard Page</h1>;
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+
+const Leaderboard = ({ users }) => {
+
+    let usersInfo = Object.values(users);
+    usersInfo.map(
+      (user) =>
+        (user.score = user.questions.length + Object.keys(user.answers).length)
+    );
+    const sortedUsers = usersInfo.sort((a, b) => b.score - a.score);
+
+    return (
+        <div>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Users</th>
+                        <th>Answered</th>
+                        <th>Created</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    {sortedUsers.map((user) => {
+                        return (
+                            <tr key={user.id}>
+                                <td>{user.name}</td>
+                                <td>{Object.keys(user.answers).length}</td>
+                                <td>{user.questions.length}</td>
+                                <td>{user.score}</td>
+                            </tr>
+                        );
+                    })}
+                </tbody>
+            </table>
+        </div>
+    );
 }
 
-export default Leaderboard;
+const mapStateToProps = ({ users }) => {
+    return {
+      users,
+    };
+}
+
+Leaderboard.propTypes = {
+    users: PropTypes.object.isRequired,
+}
+  
+export default connect(mapStateToProps)(Leaderboard);
