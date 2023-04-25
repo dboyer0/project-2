6b8b6a2f2907cc170b512ebfc38eda599615874c
dallas <dboyer@dexter.com>
2023-04-24 21:21:49 -0500

    Cleanup: removed unused files

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,11 +1,9 @@
 import { combineReducers } from "redux";
-import tests from "./tests";
 import authedUser from "./authedUser";
 import users from "./users";
 import polls from "./polls";
 
 export default combineReducers({
-    tests,
     authedUser,
     users,
     polls
